ceaf411937c2733a2865599e95db39185df8d05a
Remi Douah <remidouah@gmail.com>
2020-10-27 21:35:55 -0500

    Students

---
+++ students2020/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace students2020
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGap)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGap;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
+
+    
+
+
+
+
+
